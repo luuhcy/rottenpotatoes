bdbb31a63ca83b1932703fbf6cb5022600ee88d7
Luciana <135179274+luuhcy@users.noreply.github.com>
2025-09-23 00:34:44 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,4 @@
-class MoviesController < ApplicationController
-  def index
-    # Colunas válidas para ordenação
-    valid_sorts = %w[title release_date]
-    # Direções válidas de ordenação
-    valid_directions = %w[asc desc]
-
-    # Verifica se params[:sort] é válido, senão usa 'title' como padrão
-    sort_column = valid_sorts.include?(params[:sort]) ? params[:sort] : 'title'
-
-    # Verifica se params[:direction] é válido, senão usa 'asc' como padrão
-    sort_direction = valid_directions.include?(params[:direction]) ? params[:direction] : 'asc'
-
-    # Busca os filmes ordenados
-    @movies = Movie.order(sort_column => sort_direction)
-  end
+class ApplicationController < ActionController::Base
+  # Proteção CSRF
+  protect_from_forgery with: :exception
 end
