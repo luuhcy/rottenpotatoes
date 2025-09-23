56ea6137ed343d64535b34e989c95a78d181b6f9
Luciana <135179274+luuhcy@users.noreply.github.com>
2025-09-23 00:18:04 -0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
-class ApplicationController < ActionController::Base
-  # Protege contra ataques CSRF (Cross-Site Request Forgery)
-  protect_from_forgery with: :exception
+class MoviesController < ApplicationController
+  def index
+    # Colunas válidas para ordenação
+    valid_sorts = %w[title release_date]
+    # Direções válidas de ordenação
+    valid_directions = %w[asc desc]
 
-  # Você pode adicionar filtros ou métodos comuns a todos os controllers aqui
-  # Por exemplo, autenticação de usuário:
-  # before_action :authenticate_user!
+    # Verifica se params[:sort] é válido, senão usa 'title' como padrão
+    sort_column = valid_sorts.include?(params[:sort]) ? params[:sort] : 'title'
 
-  # Métodos de helper disponíveis nas views
-  # helper_method :current_user, :logged_in?
+    # Verifica se params[:direction] é válido, senão usa 'asc' como padrão
+    sort_direction = valid_directions.include?(params[:direction]) ? params[:direction] : 'asc'
+
+    # Busca os filmes ordenados
+    @movies = Movie.order(sort_column => sort_direction)
+  end
 end
